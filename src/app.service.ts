e37eda52208bec8f6cda7c2a07b521d7145b0ab6
Victor Olaitan <vividvictor@gmail.com>
2024-03-06 22:46:45 +0100

    Response Modification

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { AxiosResponse } from 'axios';
+import { lastValueFrom, map } from 'rxjs';
 // import { OrderDto } from './order.dto';
 @Injectable()
 export class AppService {
   constructor(private readonly httpService: HttpService) {}
 
-  async getOrderDetails(order: number): Promise<AxiosResponse> {
+  async getOrderDetailsBack(order: number): Promise<AxiosResponse> {
     // const headersRequest = {
     //   consumer_key: 'ck_15e8dbc0b41de14b24f01293e8dd2137ff08aea2',
     //   consumer_secret: 'cs_cdc6d1a737f48c7ac2f038670cfd3a4255cd274a',
@@ -23,28 +24,34 @@ export class AppService {
       });
   }
 
-  // async getOrderDetails(order: number) {
-  //   const headersRequest = {
-  //     consumer_key: 'ck_15e8dbc0b41de14b24f01293e8dd2137ff08aea2',
-  //     consumer_secret: 'cs_cdc6d1a737f48c7ac2f038670cfd3a4255cd274a',
-  //   };
+  async getOrderDetails(order: number) {
+    const headersRequest = {
+      consumer_key: 'ck_15e8dbc0b41de14b24f01293e8dd2137ff08aea2',
+      consumer_secret: 'cs_cdc6d1a737f48c7ac2f038670cfd3a4255cd274a',
+    };
 
-  //   // Axios to get order details
-  //   const response = await firstValueFrom(
-  //     this.httpService.get(
-  //       `https://socialviralclicks.com/wp-json/wc/v3/orders/${order}`,
-  //       {
-  //         headers: headersRequest,
-  //       },
-  //     ),
-  //   );
-  //   // Check if order is foreign
-  //   // if (orderDetails.meta_data[14].value === 'foreign') {
-  //   //   // Process Order on JAP
-  //   // }
-  //   console.log(`response`, response);
-  //   console.log(`Response Data`, response.data);
+    // Axios to get order details
 
-  //   // return `The order number is ${order}`;
-  // }
+    const responseData = await lastValueFrom(
+      this.httpService
+        .get(`https://socialviralclicks.com/wp-json/wc/v3/orders/${order}`, {
+          headers: headersRequest,
+        })
+        .pipe(
+          map((res) => {
+            return res.data;
+          }),
+        ),
+    );
+    return responseData;
+
+    // Check if order is foreign
+    // if (orderDetails.meta_data[14].value === 'foreign') {
+    //   // Process Order on JAP
+    // }
+    // console.log(`response`, response);
+    // console.log(`Response Data`, response.data);
+
+    // return `The order number is ${order}`;
+  }
 }
