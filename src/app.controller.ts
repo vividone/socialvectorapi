9ea5f3b61f89420a8a6649ae17ee3d39f5770fa2
Victor Olaitan <vividvictor@gmail.com>
2024-03-06 19:31:55 +0100

    Modification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-import { OrderDto } from './order.dto';
 
 @Controller()
 export class AppController {
@@ -12,17 +11,8 @@ export class AppController {
   }
 
   @Post('/order')
-  getNewOrder(@Body() orderDto: OrderDto): void {
+  getNewOrder(@Body() order: number): string {
+    return this.appService.getOrderDetails(order);
     // return console.log(orderDto);
-    const metas = orderDto.meta_data;
-
-    const product_cat = metas[14].value;
-    // const obj = metas.((k) => k.key === 'product_cat');
-    // console.log(obj);
-    if (product_cat === 'foreign') {
-      console.log(orderDto);
-    } else {
-      process.exit();
-    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,11 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getOrderDetails(order: number): string {
+    // Axios to get order details
+    // Check if order is foreign
+    // Process Order on JAP
+    return `The order number is ${order}`;
+  }
 }
